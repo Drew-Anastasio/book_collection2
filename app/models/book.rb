41d70cedd231dfe76808d7e8e6a3f49638c6b628
DrewA2000 <andrewanastasio36@gmail.com>
2023-09-12 22:42:26 -0500

    Finished BookCollection4

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+    has_many :users, through: :user_book
+    has_many :user_books
     validates :title, presence: true
     validates :author, presence: true
     validates :price, presence: true, numericality: { greater_than: 0 }
